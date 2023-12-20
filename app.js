3fe668cc1d7b5bad69cd5d61731fe54ef82d17a9
kasselrenwo <154471463+kasselrenwo@users.noreply.github.com>
2023-12-20 22:10:39 +0100

    Add files via upload

---
+++ app.js
@@ -0,0 +1,38 @@
+let tl = gsap.timeline();
+/*
+tl.to('.loading', {
+    left: '100%',
+    duration: 0.75
+})
+*/
+tl.from('.hero', {
+    duration: 0.5,
+    filter: "blur(3px)"
+})
+
+tl.from('.logo', {
+    opacity: 0,
+    duration: 0.2
+})
+
+tl.from('.socials', {
+    opacity: 0,
+    duration: 0.2
+})
+
+tl.from('.heroG h1', {
+    opacity: 0,
+    duration: 0.2
+})
+
+tl.from('.heroG p', {
+    opacity: 0,
+    duration: 0.2
+})
+
+tl.from('.heroG .btn', {
+    opacity: 0,
+    duration: 0.2
+})
+
+
